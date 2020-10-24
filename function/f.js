933294576daa8bfabf46da8a79804f573d379c89
TinfoilBat <cuentas.mantecon@gmail.com>
2020-10-24 14:53:37 +0200

    Added function to show correct cells 4 secs

--- function/f.js
+++ function/f.js
@@ -4,3 +4,17 @@ function revealColor() {
 		elements[i].style.color = 'yellow';
 	}
 }
+
+function vanishColor(){
+	let elements = document.querySelectorAll('.correct')
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.color = 'black';
+	}
+}
+
+function showCorrectCells4Seconds() {
+	revealColor();
+	setTimeout(() => {
+		vanishColor()
+	}, 4000);
+}
