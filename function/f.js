d4c0b4e97cb2ee6f80c4555aae39565ff3803a42
TinfoilBat <cuentas.mantecon@gmail.com>
2020-10-26 17:24:32 +0100

    Added solve function

--- function/f.js
+++ function/f.js
@@ -22,16 +22,32 @@ function showCorrectCells4Seconds() {
 function toogle(id) {
 	let element = document.getElementById(id);
 	if (element.className === 'toogled') {
-		element.className = '';
+		element.className = 'untoogled';
 	}
 	else {
 		element.className = 'toogled';
 	}
 }
-
 function loadToogleOnCells() {
 	let elements = document.getElementsByTagName('td');
 	for (let i = 0; i < elements.length; i++) {
 		elements[i].addEventListener("click", toogle(elements[i].id));
+	}
+}
+
+function startFanfare() {
+	loadToogleOnCells();
+	showCorrectCells4Seconds();
+}
+
+function solve() {
+	let elements = document.querySelectorAll('.toogled');
+	let corrects = document.querySelectorAll('.correct');
+
+	if (elements === corrects) {
+		return true;
+	}
+	else {
+		return false;
 	}
 }
