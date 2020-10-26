fedb3063fde4813c10618a19b622b408e8ec69c2
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-26 16:41:08 +0100

    DERROTA Y VICTORIA

--- derrota.php
+++ derrota.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VICTORIA</title>
+    <title>DERROTA</title>
+    <link rel="stylesheet" href="static/main.css">
 </head>
 <body>
     <?php include('menu.php'); 
-    echo '<h1 class="gameover"> GAME OVER</h1>'
     ?>
+
+    <div class="GAMEOVER">
+        <div class="centro">
+            <h1> GAME OVER</h1>
+            <h3> NO HAS ACERTADO TODAS LAS CASILLAS </h3>
+        </div>
+    </div>
 
 
 </body>
