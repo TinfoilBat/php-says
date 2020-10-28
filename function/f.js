f28db0aef52eb05741adb1987c8924163fb6d998
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-28 19:05:05 +0100

    Update f.js

--- function/f.js
+++ function/f.js
@@ -1,3 +1,4 @@
+// muestra de color
 function revealColor() {
 	let elements = document.querySelectorAll('.correct')
 	for (let i = 0; i < elements.length; i++) {
@@ -5,13 +6,19 @@ function revealColor() {
 	}
 }
 
+<<<<<<< HEAD
 function vanishColor() {
+=======
+// hace que vuelva al color predeterminado despues de 4 sec
+function vanishColor(){
+>>>>>>> dvd_dev
 	let elements = document.querySelectorAll('.correct');
 	for (let i = 0; i < elements.length; i++) {
 		elements[i].style.color = 'white';
 	}
 }
 
+// se muestran durantos los segundos
 function showCorrectCells4Seconds() {
 	revealColor();
 	setTimeout(() => {
@@ -19,6 +26,7 @@ function showCorrectCells4Seconds() {
 	}, 4000);
 }
 
+// funcion que da clases cuando clicas
 function toogle(id) {
 	let element = document.getElementById(id);
 	if ((!(element.classList.contains('toogled'))) || (!(element.classList.contains('untoogled')))) {
@@ -29,12 +37,24 @@ function toogle(id) {
 	}
 
 }
+
+// Recorre la tabla y hace que las celdas sean clicables
 function loadToogleOnCells() {
 	let elements = document.getElementsByTagName('td');
 	for (let i = 0; i < elements.length; i++) {
 		elements[i].addEventListener("click", toogle(elements[i].id));
 	}
 }
+<<<<<<< HEAD
+=======
+
+// carga las celdas correctas y las ilumina (fallo)
+function startFanfare() {
+	loadToogleOnCells();
+	showCorrectCells4Seconds();
+}
+
+>>>>>>> dvd_dev
 function solve() {
 	let elements = document.querySelectorAll('.toogled');
 	let corrects = document.querySelectorAll('.correct');
