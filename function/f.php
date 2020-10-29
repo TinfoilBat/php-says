1148aaa8351add5dcd1bcfb2f229f99732d53ee8
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-29 15:14:56 +0100

    nivel funcion

--- function/f.php
+++ function/f.php
@@ -1,5 +1,9 @@
 <?php
-function uniqueRandomsInClusiveRange($min, $max, $quantity)
+$nivel = 0;
+$celdas = (readFileConfig()[0][1][0] * readFileConfig()[0][1][2]);
+echo $celdas;
+
+function uniqueRandomsInClusiveRange($min, $celdas, $quantity)
 {
 	$numbers = range($min, $max);
 	shuffle($numbers);
@@ -24,3 +28,24 @@ function generateTable($height, $width, $correctCells)
 	}
 	echo '</div></table>';
 }
+
+// Devuelve una lista de listas, en la que cada lista tiene su nivel
+function readFileConfig() {
+	$Niveles = array();
+	$Fichero = file('config.txt');
+	foreach($Fichero as $linea) {
+		$separados = explode("\r\n", $linea);
+		$nivel = explode(";", $linea);
+		array_push($Niveles, $nivel);
+/*
+		echo "nivel: ";
+		print_r($nivel[0]);
+		echo "celdas: ";
+		print_r($nivel[2]);
+		echo '<br>';
+*/
+	}
+	return $x = $Niveles;
+}
+
+
