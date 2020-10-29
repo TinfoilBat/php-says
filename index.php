7f1c96047b0ec35b593997a3fbc40af8f19f1c26
TinfoilBat <cuentas.mantecon@gmail.com>
2020-10-29 16:07:26 +0100

    Autcomplete form if $_SESSION["name] is set

--- index.php
+++ index.php
@@ -1,49 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INICIO</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>INICIO</title>
 </head>
+
 <body class="letra inicio">
-    <?php
-        include('menu.php');
-    ?>
-    
-    
-    <div class="">
-        <h1 class="text-white">BIENVENIDO A MEMORY GAME</h1>
-        <pre>
+	<?php
+	session_start();
+	include('menu.php');
+	?>
+
+
+	<div class="">
+		<h1 class="text-white">BIENVENIDO A MEMORY GAME</h1>
+		<pre>
             Este juego se basa en memorizar los colores de las celdas que se enciendan, las celdas que se encienden de color amarillo
             al cabo de 4 segundos se apagaran y el usuario tendrá que seleccionar aquellas celdas que se han encendido. Si el usuario 
             ha acertado las 7 celdas que se encienden de color habrá ganado el juego.
         </pre>
-        <img src="static/MemoryGame.jpg" alt="memoryGame">
-        <h2 class="text-white">INSTRUCCIONES DE MEMORY GAME</h2>
-        <p class="letra">A continuación aparecen todos los pasos a seguir del juego: </p>
-        <ol type="1">
-            <li>Primero de todo darle al botón de iniciar.</li>
-            <li>Esperar mientras se muestran las celdas de color amarillo.</li>
-            <li>Una vez se han apagado seleccionar las celdas que se han encendido.</li>
-            <li>Cuando ya se hayan seleccionado las 7 celdas que se han encendido anteriormente darle al botón resolver.</li>
-            <li>Después de darle al botón de resolver aparecera la pantalla de derrota o victoria correspondiente.</li>
-            <li>El juego ya se ha terminado, si quieres empezar de nuevo darle al botón de HOME (arriba a la derecha) y volver a iniciar la partida.</li>
-        </ol>
+		<img src="static/MemoryGame.jpg" alt="memoryGame">
+		<h2 class="text-white">INSTRUCCIONES DE MEMORY GAME</h2>
+		<p class="letra">A continuación aparecen todos los pasos a seguir del juego: </p>
+		<ol type="1">
+			<?php
+			echo "SESSION variable: " . $_SESSION["nombre"];
+			?>
+			<li>Primero de todo darle al botón de iniciar.</li>
+			<li>Esperar mientras se muestran las celdas de color amarillo.</li>
+			<li>Una vez se han apagado seleccionar las celdas que se han encendido.</li>
+			<li>Cuando ya se hayan seleccionado las 7 celdas que se han encendido anteriormente darle al botón resolver.</li>
+			<li>Después de darle al botón de resolver aparecera la pantalla de derrota o victoria correspondiente.</li>
+			<li>El juego ya se ha terminado, si quieres empezar de nuevo darle al botón de HOME (arriba a la derecha) y volver a iniciar la partida.</li>
+		</ol>
+		<div class="centro">
+			<form method="POST" action="game.php">
+				<div>
+					<label for="nombre" minlength="1">Introduce tu nombre: </label>
+				</div>
+				<div class="espaciado">
+					<?php
+					if (isset($_SESSION["nombre"])) {
+						echo "<input type=\"text\" name=\"nombre\" value=\"{$_SESSION["nombre"]}\"required></input>";
+					} else {
+						echo "<input type=\"text\" name=\"nombre\" required></input>";
+					}
+					?>
+				</div>
+				<div>
+					<button class="boton"> Enviar</button>
+				</div>
+			</form>
+		</div>
+	</div>
 
-        <div class="centro">
-            <form method="POST" action="game.php"> 
-                <div>
-                    <label for="nombre" minlength="1">Introduce tu nombre: </label>
-                </div>
-                <div class="espaciado">
-                    <input type="text" name="nombre" required></input>
-                </div>
-                <div>
-                    <button class="boton"> Enviar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-    
+
 </body>
+
 </html>
