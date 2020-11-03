1a724acb5fa9db10530d1050f989fb787ff9bb2f
TinfoilBat <cuentas.mantecon@gmail.com>
2020-11-03 18:42:09 +0100

    Added hrefs to buttons. Missing next level capability

--- victoria.php
+++ victoria.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VICTORIA</title>
-    <link rel="stylesheet" href="styles/main.css">
-    <link rel="stylesheet" href="styles/win.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>VICTORIA</title>
+	<link rel="stylesheet" href="styles/main.css">
+	<link rel="stylesheet" href="styles/win.css">
 
 </head>
+
 <body>
-    <?php include('menu.php'); 
-    ?>
+	<?php include('menu.php');
+	?>
 
-    <div class="win">
-        <div class="centro">
-            <h1> HAS GANADO</h1>
-            <h3> HAS ACERTADO TODAS LAS CASILLAS </h3>
-        </div>
-    </div>
-
+	<div class="win">
+		<div class="centro">
+			<h1> HAS GANADO</h1>
+			<h3> HAS ACERTADO TODAS LAS CASILLAS </h3>
+		</div>
+	</div>
+	<?php
+	include('post_game_buttons.php') ?>
 </body>
+
 </html>
