65ca121edaebaeed647f4c0dad86ab3604ba7fc0
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-09 17:59:49 +0100

    menu y index css and responsive

--- common/header.php
+++ common/header.php
@@ -1,14 +1,17 @@
 <style><?php include_once(dirname(__DIR__).'/styles/style.css');?></style> 
-<div>
-	<div class="menu">
+<div class="menu">
+
+	<div class="flex">
 		<?php
-		if (($_SERVER['REQUEST_URI'] === '/php-says/index.php') || ($_SERVER['REQUEST_URI'] === '/php-says/')) {
-			echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
-			echo "<button onclick=\"window.location.href='/php-says/src/ranking.php'\" accesskey='r'><u>R</u>ANKING</button>";
-		 } else {
-			echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
-			}
-		
-		?>
+			if (($_SERVER['REQUEST_URI'] === '/php-says/index.php') || ($_SERVER['REQUEST_URI'] === '/php-says/')) {
+				echo '<div class="icon"><img src="static/icon.png" width="45" margin="0" class="" alt="memoryGame"></div>';
+				echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
+				echo "<button onclick=\"window.location.href='/php-says/src/ranking.php'\" accesskey='r'><u>R</u>ANKING</button>";
+			} else {
+				echo '<div class="icon"><img src="../static/icon.png" width="45" margin="0" class="" alt="memoryGame"></div>';
+				echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
+				}
+			
+			?>
 	</div>
 </div>
