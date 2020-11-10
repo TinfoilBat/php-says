cbb82cb7aa9539cab0970faae962b97fba545eaf
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-10 22:16:22 +0100

    Responsive + daltonico en menu

--- common/header.php
+++ common/header.php
@@ -7,9 +7,12 @@
 				echo '<div class="icon"><img src="static/icon.png" width="45" margin="0" class="" alt="memoryGame"></div>';
 				echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
 				echo "<button onclick=\"window.location.href='/php-says/src/ranking.php'\" accesskey='r'><u>R</u>ANKING</button>";
+				echo "<button class='daltonicob' onclick=\"\" accesskey='d'><u>D</u>ALTONICO</button>";
+
 			} else {
 				echo '<div class="icon"><img src="../static/icon.png" width="45" margin="0" class="" alt="memoryGame"></div>';
 				echo "<button onclick=\"window.location.href='/php-says/index.php'\" accesskey='i'><u>I</u>NICIO</button>";
+				echo "<button class='daltonicob' onclick=\"\" accesskey='d'><u>D</u>ALTONICO</button>";
 				}
 			
 			?>
