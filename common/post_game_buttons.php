95d1af942699102fa2332b331112f911e8135382
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-11 21:55:53 +0100

    d

--- common/post_game_buttons.php
+++ common/post_game_buttons.php
@@ -3,12 +3,12 @@
 	<?php include_once(dirname(__DIR__) . '/styles/style.css'); ?>
 </style>
 <!-- <p>Codigo de nivel!!</p> ToDo -->
-<div class="botones">
-	</div>
+<div class="botonesflex">
+	
 	<?php
 	if ($_SERVER['REQUEST_URI'] === '/php-says/src/derrota.php') {
 		$_SESSION['tries']++;
-		echo
+		echo 
 			"<div>
 				<form action='./game.php' method='POST'>
 					<button id='daltonico' type='submit' name='nombre' value=\"{$_SESSION['playername']}\" accesskey='r'><u>R</u>EPETIR</button>
@@ -32,6 +32,10 @@
 
 		echo "<form action='./game.php' method='POST'>
 			<button id='daltonico' type='submit' name='next' accesskey='s' value=" . $_SESSION['level']++	 . "><u>S</u>IGUIENTE</button>		</form>
+		</div>";
+		echo "<form action='./ranking.php' method='POST'>
+			<input style=\"display:none\" type='text' name='final_time' id='final_time' value=\"{$_SESSION['victory_final_time']}\">
+			<button id='daltonico' type='submit' name='next' accesskey='s'>ACABAR</button>		</form>
 		</div>";
 	}
 	?>
